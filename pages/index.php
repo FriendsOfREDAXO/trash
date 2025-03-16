0d683a1f9cbadb35d8e0ef791e8a86aae06aca54
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-16 21:30:21 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,4 @@
 <?php
 $package = rex_addon::get('trash');
-echo rex_view::title(rex_i18n::msg('trash_trash'));
+echo rex_view::title(rex_i18n::msg('trash'));
 rex_be_controller::includeCurrentPageSubPath();
