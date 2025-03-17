a7b120bbf344c7f5be081802810da179d0a78a8a
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-17 22:08:32 +0100

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+try {
+    // Tabellen für gelöschte Artikel und deren Slices löschen
+    rex_sql_table::get(rex::getTable('trash_article'))->drop();
+    rex_sql_table::get(rex::getTable('trash_article_slice'))->drop();
+    
+    // Cache löschen
+    rex_delete_cache();
+    
+    return true;
+} catch (rex_sql_exception $e) {
+    $addon = rex_addon::get('trash');
+    $addon->setProperty('installmsg', $e->getMessage());
+    return false;
+}
+  ?>
